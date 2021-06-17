56f22ea3a52959be8d438cab2188eddcd9cc32b5
tinycoder2123 <70987474+tinycoder2123@users.noreply.github.com>
2021-06-17 13:26:16 -0600

    console application that prompts the user to enter a regular expression
    Create a console application that prompts the user to enter a regular expression, and then prompts the user to enter some input and compare the two for a match until the user presses Esc:
    
    The default regular expression checks for at least one digit.
    Enter a regular expression (or press ENTER to use the default): ^[a- z]+$
    Enter some input: apples
    apples matches ^[a-z]+$? True
    Press ESC to end or any key to try again.
    Enter a regular expression (or press ENTER to use the default): ^[a- z]+$
    Enter some input: abc123xyz
    abc123xyz matches ^[a-z]+$? False
    Press ESC to end or any key to try again.

---
+++ Module1Assignment1.2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Module1Assignment1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputRegex = "";
+            string inputString = "";
+
+            Console.WriteLine("The default regular expression checks for at least one digit.");
+
+            while (true)
+            {
+                Console.Write("\nEnter a regular expression (or press ENTER to use the default): ");
+                inputRegex = Console.ReadLine();
+                if (inputRegex == "")
+                    inputRegex = "[0-9]+";
+                Console.Write("Enter some input: ");
+                inputString = Console.ReadLine();
+                bool isMatchFound = Regex.IsMatch(inputString, inputRegex);
+                Console.WriteLine("{0} matches {1} ? {2}", inputString, inputRegex, isMatchFound);
+                Console.Write("\nPress ESC to end or any key to try again.");
+                ConsoleKeyInfo key = Console.ReadKey();
+                if (key.Key == ConsoleKey.Escape)
+                    break;
+            }
+        }
+    }
+}
